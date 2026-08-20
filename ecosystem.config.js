e86027e269ff861ce1eec96555ed7b28c3ceae04
MoisesAzevedo <MoisesDev2022@gmail.com>
2026-08-20 15:54:30 -0300

    add ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   apps: [{
     name: "doc-docusaurus",
-    script: "npm",
-    args: "run serve -- --host 0.0.0.0 --port 3000",
-    interpreter: "none"
+    script: "cmd.exe",
+    args: "/c npm run serve -- --host 0.0.0.0 --port 3000"
   }]
 }
