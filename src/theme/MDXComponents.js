ac306860a19b171ef359eb015a744f280c8fc613
MoisesAzevedo <MoisesDev2022@gmail.com>
2026-08-20 17:26:35 -0300

    FIx(docaccordeon) [accoredons internos com altura bugada]:  adicionando novo componente para os accoredeons internos para ter o controle de altura individual.

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,11 +1,13 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import DocAccordion from '@site/src/components/DocAccordion/DocAccordion';
+import DocAccordionInner from '@site/src/components/DocAccordion/DocAccordionInner/DocAccordionInner';
 import ExpandAllControls from '@site/src/components/DocAccordion/ExpandAllControls';
 import ImageLightbox from '@site/src/components/ImageLightbox/ImageLightbox';
 
 export default {
   ...MDXComponents,
   DocAccordion,
+  DocAccordionInner,
   ExpandAllControls,
   ImageLightbox,
 };
